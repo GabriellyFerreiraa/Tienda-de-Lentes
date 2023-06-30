d7b05a7931d56fe96ecfab6769fa69770063f659
Gabrielly Ferreira <gabiferreira@hotmail.com>
2023-06-29 21:48:33 -0300

    new change

---
+++ myecomerce/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+export const NavBar = () => (
+    <header>
+        <nav>
+            <h6>Tienda de lentes</h6>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                    <a href="#">Productos</a>
+                    <a href="#">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+)
